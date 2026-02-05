54d1ffeff1ca179482144295779a94de2323eb49
Ahmad Amiri <amiri@jibit.ir>
2026-02-05 18:00:48 +0330

    first commit

---
+++ src/test/java/com/azki/example/reservation/ReservationApplicationTests.java
@@ -0,0 +1,15 @@
+package com.azki.example.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class ReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
